1b723da506f05f6984d960f3ca5baf7d695c19c2
tekater <120014627+tekater@users.noreply.github.com>
2023-04-26 19:53:10 +0900

    Update doggy.cpp

--- doggy.cpp
+++ doggy.cpp
@@ -9,10 +9,11 @@
 using namespace std;
 
 struct Dogs {
-    char Name[20];
-    int Class;
-    char stepName[20];
+    char Name[20]={" "};
+    int Class = 0;
+    char stepName[20] = { " " };
 };
+
 void Show(Dogs* dog, int length) {
     for (int i = 0; i < length; i++) {
         cout << "Dog: " << i + 1 << " ";
@@ -22,43 +23,55 @@ void Show(Dogs* dog, int length) {
         cout << endl;
     }
 }
+
 void Bark() {
     cout << "Гав";
 }
-void Cinput(Dogs* dog, int length) {
-    for (int i = 0; i < length; i++) {
-        cout << "Dog: " << i + 1 << "\n";
-        cout << "Name: "; cin >> dog[i].Name; cout << endl;
-        cout << "Class(int): "; cin >> dog[i].Class; cout << endl;
-        cout << "StepName: "; cin >> dog[i].stepName; cout << endl;
+
+void Cinput(Dogs* dog, int chose) {
+        cout << "Dog: " << chose << "\n";
+        chose -= 1;
+        cout << "Name: "; cin >> dog[chose].Name; cout << endl;
+        cout << "Class(int): "; cin >> dog[chose].Class; cout << endl;
+        cout << "StepName: "; cin >> dog[chose].stepName; cout << endl;
         cout << endl;
-    }
 }
+
 void Doggy(Dogs* dog,int length) {
-    int chose;
+    int chose; int c = 0;
     do {
-        system("cls");
-    cout << "\n1 - Заполнение объектов.\n2 - Вывод\n3 - Подать голос\n4 - Выход\n";
+    system("cls");
+    cout << "\n1 - Заполнение объектов.\n2 - Вывод\n3 - Подать голос\n4 - Пополнить список Собак\n5 - Выход\n";
     cout << "\nВыберите действие: "; cin >> chose;
     switch (chose) {
     case 1:
-        Cinput(dog, length);
+        cout << "Информацию о какой собаке вы хотите заполнить ? 1 - " << length << "\n";
+        cin >> c;
+        Cinput(dog, c);
         break;
     case 2:
         Show(dog, length);
-        Sleep(1000);
+        Sleep(2000);
         break;
     case 3:
         Bark();
         Sleep(1000);
+        break;
+    case 4:
+        cout << "\nСколько прибыло собак ?\n";
+        int many; many = 0;
+        cin >> many;
+        length += many;
         break;
     default:
         cout << "Выход :)";
         Sleep(1000);
         break;
     }
-    } while (chose != 4);
+
+    } while (chose != 5);
 }
+
 int main()
 {
     setlocale(0, "");
@@ -66,6 +79,7 @@ int main()
     //system("color B5");
     cout << "Сколько собак будет?\n";
     int a = 0; cin >> a;
-    Dogs* dogs = new Dogs[a];
+    Dogs* dogs = new Dogs[100];
     Doggy(dogs,a);
+
 }
